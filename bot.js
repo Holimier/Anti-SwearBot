0dd6fc61f06f28798b23f9306d9076d8387dad36
Holimier <37423453+Holimier@users.noreply.github.com>
2018-03-16 15:37:47 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,68 @@
+console.log('Cargando Bot...!')
+var Discord = require('discord.js')
+var bot = new Discord.Client();
+var fs = require("fs");
+var profanities = require("profanities")
+
+bot.on('message', (message) => {
+
+    if (message.channel.type === 'dm') return;
+
+
+    var sender = message.author;
+    var msg = message.content.toUpperCase();
+    var prefix = "+"
+    
+    
+    for (x = 0; x < profanities.length; x++) {
+        if (msg.includes(profanities[x].toUpperCase())) {
+            message.channel.send("Don\'t say that!")
+            message.delete();
+            return;
+        }
+    }
+
+    if (msg === prefix + "PING") {
+        message.channel.send("pong!")
+    }
+
+    
+
+    if ((sender.id == "357500239793618955" || sender.id == "238038678071410689") && sender.id != "423609041583865877" && msg.includes(prefix + "AW")) {
+        var archivo = fs.readFileSync("node_modules/profanities/index.json");
+        var badword = msg.split(" ");
+
+        var badWords = String(archivo);
+        var editarchivo = badWords.split("]") //Archivo final
+
+        console.log(badWords);
+        console.log(editarchivo[0]);
+
+        var badwordfinal = badword[1];
+        console.log(badword[1]);
+
+        var editfinal = editarchivo[0] + "," + JSON.stringify(badword[1]) + "\n]"
+        console.log(editfinal);
+
+        fs.writeFile("node_modules/profanities/index.json", (editfinal), (error) => {
+            if (error) console.log(error)});
+        message.channel.send("Success!")
+    }
+
+
+
+    if (sender.id === "423609041583865877") {
+        return;
+    }
+
+    if (msg.includes("PUTO")) {
+        message.channel.send("Puto tu :v")
+    }
+
+    
+});
+bot.on("ready", () => {
+    console.log("Listo!")
+    bot.user.setUsername("Anti-SwearBot");
+});
+client.login(process.env.BOT_TOKEN);
